ba0593cb5fa72cca52ecabaf61ccfc8d5077921b
dsw7 <dds3443@hotmail.com>
2025-06-23 18:07:50 -0700

    Fix JSON parsing issues

--- src/query_openai.cpp
+++ src/query_openai.cpp
@@ -7,6 +7,49 @@
 
 namespace {
 
+std::string get_stringified_json_from_completion(const std::string &completion)
+{
+    /*
+     * Some models return a JSON completion without triple backticks:
+     * {
+     *   k: v,
+     * }
+     * Others return a JSON completion with triple backticks:
+     * ```json
+     * {
+     *   k: v,
+     * }
+     * ```
+     */
+
+    if (completion[0] == '{' and completion.back() == '}') {
+        return completion;
+    }
+
+    bool append_enabled = false;
+    std::string line;
+    std::stringstream ss(completion);
+    std::string raw_json;
+
+    while (std::getline(ss, line)) {
+        if (line == "```json") {
+            append_enabled = true;
+        } else if (line == "```") {
+            append_enabled = false;
+        } else {
+            if (append_enabled) {
+                raw_json += line;
+            }
+        }
+    }
+
+    if (append_enabled) {
+        throw std::runtime_error("Closing triple backticks not found. Raw JSON might be malformed");
+    }
+
+    return raw_json;
+}
+
 std::string serialize_request(const std::string &prompt, const std::string &model)
 {
     const nlohmann::json messages = { { "role", "developer" }, { "content", prompt } };
@@ -38,10 +81,12 @@ query_openai::QueryResults deserialize_response(const std::string &response)
     }
 
     const std::string content = json["choices"][0]["message"]["content"];
+    const std::string raw_json = get_stringified_json_from_completion(content);
+
     nlohmann::json json_content;
 
     try {
-        json_content = nlohmann::json::parse(content);
+        json_content = nlohmann::json::parse(raw_json);
     } catch (const nlohmann::json::parse_error &e) {
         throw std::runtime_error(fmt::format("Failed to parse content: {}", e.what()));
     }
