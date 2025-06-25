e336248a8d1c1ea63e478667c03305876581061b
dsw7 <dds3443@hotmail.com>
2025-06-25 01:53:35 -0700

    Complete file I/O class

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -9,7 +9,33 @@ namespace {
 
 const std::string DELIMITER_LINE_ = "@@@@@\n";
 
+struct Parts {
+    std::string head;
+    std::string core;
+    std::string tail;
+};
+
+Parts unpack_text_into_parts(const std::string &text)
+{
+    const std::string::size_type idx_head = text.find(DELIMITER_LINE_);
+
+    Parts parts;
+    parts.head = text.substr(0, idx_head);
+
+    const std::string::size_type idx_start_core = idx_head + 6;
+    const std::string::size_type idx_tail = text.find(DELIMITER_LINE_, idx_start_core);
+
+    if (idx_tail == std::string::npos) {
+        throw std::runtime_error("No matching closing delimiter line");
+    }
+
+    const std::string::size_type size_core = idx_tail - idx_start_core;
+    parts.core = text.substr(idx_start_core, size_core);
+    parts.tail = text.substr(idx_tail + 6);
+    return parts;
 }
+
+} // namespace
 
 namespace file_io {
 
@@ -26,24 +52,65 @@ void FileIO::load_input_text(const std::filesystem::path &filename)
     const std::string file_contents = utils::read_from_file(filename);
 
     if (file_contents.contains(DELIMITER_LINE_)) {
+        const Parts parts = unpack_text_into_parts(file_contents);
+        this->head_ = parts.head;
+        this->core_ = parts.core;
+        this->tail_ = parts.tail;
         this->is_delimited_ = true;
     } else {
-        this->is_delimited_ = false;
         this->text_ = file_contents;
+        this->is_delimited_ = false;
+    }
+}
+
+std::string FileIO::get_text()
+{
+    if (this->is_delimited_) {
+        if (not this->core_) {
+            throw std::logic_error("The [core_] variable is unset!");
+        }
+
+        return this->core_.value();
+    }
+
+    if (not this->text_) {
+        throw std::logic_error("The [text_] variable is unset!");
+    }
+
+    return this->text_.value();
+}
+
+void FileIO::set_text(const std::string &text)
+{
+    if (this->is_delimited_) {
+        this->core_ = text;
+    } else {
+        this->text_ = text;
     }
 }
 
 void FileIO::dump_output_text(const std::filesystem::path &filename)
 {
-    if (not this->is_delimited_) {
-        if (this->text_) {
-            utils::write_to_file(filename, this->text_.value());
-        } else {
-            throw std::logic_error("The [text_] variable is unset!");
+    if (this->is_delimited_) {
+        if (not this->head_) {
+            throw std::logic_error("The [head_] variable is unset!");
+        }
+        if (not this->core_) {
+            throw std::logic_error("The [core_] variable is unset!");
+        }
+        if (not this->tail_) {
+            throw std::logic_error("The [tail_] variable is unset!");
         }
 
+        utils::write_to_file(filename, this->head_.value() + this->core_.value() + this->tail_.value());
         return;
     }
+
+    if (not this->text_) {
+        throw std::logic_error("The [text_] variable is unset!");
+    }
+
+    utils::write_to_file(filename, this->text_.value());
 }
 
 } // namespace file_io
--- src/file_io.hpp
+++ src/file_io.hpp
@@ -9,16 +9,16 @@ namespace file_io {
 class FileIO {
 private:
     bool is_delimited_ = false;
-    std::string core_;
-    std::string head_;
-    std::string tail_;
+    std::optional<std::string> core_;
+    std::optional<std::string> head_;
+    std::optional<std::string> tail_;
     std::optional<std::string> text_;
 
 public:
     void load_input_text(const std::filesystem::path &filename);
-    void dump_output_text(const std::filesystem::path &filename);
     std::string get_text();
     void set_text(const std::string &text);
+    void dump_output_text(const std::filesystem::path &filename);
 };
 
 } // namespace file_io
