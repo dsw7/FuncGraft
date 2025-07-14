6b802700e63c0b50f978d4ff8b1c3027f15fa76f
dsw7 <dds3443@hotmail.com>
2025-07-14 01:19:14 -0700

    Add test for wrong model

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -57,6 +57,20 @@ class TestMisc(TestCase):
             process.stderr,
         )
 
+    def test_wrong_model(self) -> None:
+        command = [
+            get_gpe_binary(),
+            LOC_TEST_DATA / "dummy_basic.py",
+            "--instructions=A foo that bars",
+            "--model=tts-1-hd",
+        ]
+        process = run(command, stdout=PIPE, stderr=PIPE, text=True)
+        self.assertEqual(process.returncode, 1)
+        self.assertIn(
+            "This is not a chat model and thus not supported in the v1/chat/completions endpoint. Did you mean to use v1/completions?",
+            process.stderr,
+        )
+
     def test_empty_instructions(self) -> None:
         command = [
             get_gpe_binary(),
