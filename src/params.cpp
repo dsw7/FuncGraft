e9b1eee8dcc57b929689b3348d9b79b3243d8e8b
dsw7 <dds3443@hotmail.com>
2025-07-24 02:03:01 -0700

    Move model check

--- src/params.cpp
+++ src/params.cpp
@@ -9,6 +9,10 @@ void CommandLineParameters::validate_params()
     if (this->input_file.empty()) {
         throw std::invalid_argument("No filename was provided. Cannot proceed");
     }
+
+    if (this->model.empty()) {
+        throw std::invalid_argument("Model is empty. Cannot proceed");
+    }
 }
 
 } // namespace params
