af30fb5166b2facde485d2bd0a47316ecae971d4
dsw7 <dds3443@hotmail.com>
2025-11-14 02:58:25 -0800

    Add test case

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from subprocess import run, PIPE
 from unittest import TestCase
 from .utils import get_gpe_binary
@@ -30,3 +31,10 @@ class TestMisc(TestCase):
     #        self.assertIn(
     #            "No instructions provided by file or command line", process.stderr
     #        )
+
+    def test_copyright(self) -> None:
+        proc = self.assertSuccess("--help")
+        self.assertIn(
+            f"-- FuncGraft | Copyright (C) {datetime.now().year} by David Weber",
+            proc.stdout.strip(),
+        )
