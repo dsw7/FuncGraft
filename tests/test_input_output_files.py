19fc50768b5472d32b4d971a06c417faf0168181
dsw7 <dds3443@hotmail.com>
2026-01-05 02:54:23 -0800

    Migrate test_input_output_files.py

--- tests/test_input_output_files.py
+++ tests/test_input_output_files.py
@@ -1,22 +1,22 @@
-from .utils import LOC_TEST_DATA, ExtendedTestCase
+from .utils import LOC_TEST_DATA, assert_command_failure
 
 
-class TestInputFileOptions(ExtendedTestCase):
-    def test_input_file_is_empty(self) -> None:
-        stderr = self.assertFailure("")
-        self.assertIn("No filename was provided. Cannot proceed", stderr)
-
-    def test_input_file_is_not_a_file(self) -> None:
-        stderr = self.assertFailure("/tmp", "-iFoo")
-        self.assertIn("Input '/tmp' is not a file!", stderr)
-
-    def test_input_file_is_missing(self) -> None:
-        stderr = self.assertFailure("abc.txt", "-iFoo")
-        self.assertIn("File 'abc.txt' does not exist!", stderr)
+def test_input_file_is_empty() -> None:
+    stderr = assert_command_failure("")
+    assert "No filename was provided. Cannot proceed" in stderr
 
 
-class TestOutputFileOptions(ExtendedTestCase):
-    def test_output_file_is_empty(self) -> None:
-        path_file = str(LOC_TEST_DATA / "dummy_basic.py")
-        stderr = self.assertFailure(path_file, "--output=")
-        self.assertIn("Output filename was not provided. Cannot proceed", stderr)
+def test_input_file_is_not_a_file() -> None:
+    stderr = assert_command_failure("/tmp", "-iFoo")
+    assert "Input '/tmp' is not a file!" in stderr
+
+
+def test_input_file_is_missing() -> None:
+    stderr = assert_command_failure("abc.txt", "-iFoo")
+    assert "File 'abc.txt' does not exist!" in stderr
+
+
+def test_output_file_is_empty() -> None:
+    path_file = str(LOC_TEST_DATA / "dummy_basic.py")
+    stderr = assert_command_failure(path_file, "--output=")
+    assert "Output filename was not provided. Cannot proceed" in stderr
