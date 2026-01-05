9f579c64f190ee6d8b125dc0af7e562ce6f8bb5a
dsw7 <dds3443@hotmail.com>
2026-01-05 03:26:39 -0800

    Migrate test_edit.py

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -1,83 +1,87 @@
 from os import remove
 from subprocess import run, PIPE
+from typing import Generator
+from pytest import fixture
 from .utils import LOC_TEST_DATA, assert_command_success, assert_command_failure
 
-OUTPUT_PATH = "/tmp/dummy_basic.py"
 
+@fixture
+def outputted_script() -> Generator[str, None, None]:
+    path_to_outputted_script = "/tmp/dummy_basic.py"
+    yield path_to_outputted_script
 
-def tearDown() -> None:
     try:
-        remove(OUTPUT_PATH)
+        remove(path_to_outputted_script)
     except FileNotFoundError:
         pass
 
 
-def run_temp_python_script() -> str:
-    # some tests will generate and edit OUTPUT_PATH python script with LLMs
+def run_outputted_python_script(python_script: str) -> str:
+    # some tests will generate and edit a python script with LLMs
     # run this script to see if it works after modification by an LLM
-    process = run(["python3", OUTPUT_PATH], stdout=PIPE, stderr=PIPE, text=True)
+    process = run(["python3", python_script], stdout=PIPE, stderr=PIPE, text=True)
     assert process.returncode == 0, process.stderr
     return process.stdout
 
 
-def test_read_instructions_from_file() -> None:
+def test_read_instructions_from_file(outputted_script: str) -> None:
     assert_command_success(
         str(LOC_TEST_DATA / "dummy_basic.py"),
         f'--file={LOC_TEST_DATA / "edit.txt"}',
-        f"-o{OUTPUT_PATH}",
+        f"-o{outputted_script}",
     )
-    assert "The sum is 14" in run_temp_python_script()
+    assert "The sum is 14" in run_outputted_python_script(outputted_script)
 
 
-def test_read_instructions_from_cli() -> None:
+def test_read_instructions_from_cli(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 5"
     stdout = assert_command_success(
         str(LOC_TEST_DATA / "dummy_basic.py"),
         f"--instructions='{instructions}'",
-        f"-o{OUTPUT_PATH}",
+        f"-o{outputted_script}",
     )
     assert "Prompt:" not in stdout  # Prompt should not print by default
-    assert "The sum is 9" in run_temp_python_script()
+    assert "The sum is 9" in run_outputted_python_script(outputted_script)
 
 
-def test_print_prompt_with_verbose_flag() -> None:
+def test_print_prompt_with_verbose_flag(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 5"
     stdout = assert_command_success(
         str(LOC_TEST_DATA / "dummy_basic.py"),
         f"--instructions='{instructions}'",
-        f"-o{OUTPUT_PATH}",
+        f"-o{outputted_script}",
         "--verbose",
     )
     assert "Prompt:" in stdout
-    assert "The sum is 9" in run_temp_python_script()
+    assert "The sum is 9" in run_outputted_python_script(outputted_script)
 
 
-def test_only_edit_between_delims() -> None:
+def test_only_edit_between_delims(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 3"
     assert_command_success(
         str(LOC_TEST_DATA / "dummy_with_delims.py"),
         f"--instructions='{instructions}'",
-        f"-o{OUTPUT_PATH}",
+        f"-o{outputted_script}",
     )
-    assert "The sum is 17" in run_temp_python_script()
+    assert "The sum is 17" in run_outputted_python_script(outputted_script)
 
 
-def test_bad_delim_placement() -> None:
+def test_bad_delim_placement(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 3"
     stderr = assert_command_failure(
         str(LOC_TEST_DATA / "dummy_with_bad_delims.py"),
         f"--instructions='{instructions}'",
-        f"-o{OUTPUT_PATH}",
+        f"-o{outputted_script}",
     )
     assert "No matching closing delimiter line" in stderr
 
 
-def test_bad_delim_placement_2() -> None:
+def test_bad_delim_placement_2(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 3"
     stderr = assert_command_failure(
         str(LOC_TEST_DATA / "dummy_with_bad_delims_2.py"),
         f"--instructions='{instructions}'",
-        f"-o{OUTPUT_PATH}",
+        f"-o{outputted_script}",
     )
     assert "The number of delimiter lines must be exactly 2" in stderr
 
@@ -108,13 +112,13 @@ def test_work_with_empty_delims_2() -> None:
     assert "The delimited block does not contain any code" in stderr
 
 
-def test_work_on_unknown_file() -> None:
+def test_work_on_unknown_file(outputted_script: str) -> None:
     instructions = "Capitalize all words in the file"
     assert_command_success(
         str(LOC_TEST_DATA / "unknown_file_type.abc"),
         f"--instructions='{instructions}'",
-        f"-o{OUTPUT_PATH}",  # Will write to a .py file but it's okay
+        f"-o{outputted_script}",  # Will write to a .py file but it's okay
     )
 
-    with open(OUTPUT_PATH) as f:
+    with open(outputted_script) as f:
         assert "Foobar" in f.read()
