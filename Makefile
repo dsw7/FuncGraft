9aca9470aba453492aac2988cf56785f5c69ddd2
dsw7 <dds3443@hotmail.com>
2026-01-06 00:25:18 -0800

    Add ollama tests

--- Makefile
+++ Makefile
@@ -22,7 +22,9 @@ test: export PATH_BIN = $(CURDIR)/build/test/edit
 test: format
 	@cmake -S src -B build/test -DENABLE_TESTING=ON -DENABLE_COVERAGE=ON
 	@make --jobs=12 --directory=build/test
-	@python3 -m pytest -vs tests/
+	@python3 -m pytest -vs -m test_ollama tests/
+	@python3 -m pytest -vs -m test_misc tests/
+	@python3 -m pytest -vs -m test_openai tests/
 	@lcov --capture --directory=build/test --output-file build/test/coverage.info
 	@lcov --remove build/test/coverage.info "/usr/*" "*/external/*" --output-file build/test/coverage.info
 	@genhtml build/test/coverage.info --output-directory build/test/coverageResults
---
+++ tests/test_edit_ollama.py
@@ -0,0 +1,128 @@
+from pytest import mark
+from utils import (
+    LOC_TEST_DATA,
+    assert_command_success,
+    assert_command_failure,
+    assert_python_script_runs,
+)
+
+
+@mark.test_ollama
+def test_read_instructions_from_file(outputted_script: str) -> None:
+    assert_command_success(
+        str(LOC_TEST_DATA / "dummy_basic.py"),
+        f'--file={LOC_TEST_DATA / "edit.txt"}',
+        f"-o{outputted_script}",
+        "--use-local",
+    )
+    assert "The sum is 14" in assert_python_script_runs(outputted_script)
+
+
+@mark.test_ollama
+def test_read_instructions_from_cli(outputted_script: str) -> None:
+    instructions = "Replace the variable `c` with the integer 5"
+    stdout = assert_command_success(
+        str(LOC_TEST_DATA / "dummy_basic.py"),
+        f"--instructions='{instructions}'",
+        f"-o{outputted_script}",
+        "--use-local",
+    )
+    assert "Prompt:" not in stdout  # Prompt should not print by default
+    assert "The sum is 9" in assert_python_script_runs(outputted_script)
+
+
+@mark.test_ollama
+def test_print_prompt_with_verbose_flag(outputted_script: str) -> None:
+    instructions = "Replace the variable `c` with the integer 5"
+    stdout = assert_command_success(
+        str(LOC_TEST_DATA / "dummy_basic.py"),
+        f"--instructions='{instructions}'",
+        f"-o{outputted_script}",
+        "--verbose",
+        "--use-local",
+    )
+    assert "Prompt:" in stdout
+    assert "The sum is 9" in assert_python_script_runs(outputted_script)
+
+
+@mark.test_ollama
+def test_only_edit_between_delims(outputted_script: str) -> None:
+    instructions = "Replace the variable `c` with the integer 3"
+    assert_command_success(
+        str(LOC_TEST_DATA / "dummy_with_delims.py"),
+        f"--instructions='{instructions}'",
+        f"-o{outputted_script}",
+        "--use-local",
+    )
+    assert "The sum is 17" in assert_python_script_runs(outputted_script)
+
+
+@mark.test_ollama
+def test_bad_delim_placement(outputted_script: str) -> None:
+    instructions = "Replace the variable `c` with the integer 3"
+    stderr = assert_command_failure(
+        str(LOC_TEST_DATA / "dummy_with_bad_delims.py"),
+        f"--instructions='{instructions}'",
+        f"-o{outputted_script}",
+        "--use-local",
+    )
+    assert "No matching closing delimiter line" in stderr
+
+
+@mark.test_ollama
+def test_bad_delim_placement_2(outputted_script: str) -> None:
+    instructions = "Replace the variable `c` with the integer 3"
+    stderr = assert_command_failure(
+        str(LOC_TEST_DATA / "dummy_with_bad_delims_2.py"),
+        f"--instructions='{instructions}'",
+        f"-o{outputted_script}",
+        "--use-local",
+    )
+    assert "The number of delimiter lines must be exactly 2" in stderr
+
+
+@mark.test_ollama
+def test_work_on_empty_file() -> None:
+    instructions = "Replace the variable `c` with the integer 3"
+    stderr = assert_command_failure(
+        str(LOC_TEST_DATA / "dummy_empty.py"),
+        f"--instructions='{instructions}'",
+        "--use-local",
+    )
+    assert "The file does not contain any code" in stderr
+
+
+@mark.test_ollama
+def test_work_with_empty_delims() -> None:
+    instructions = "Replace the variable `c` with the integer 3"
+    stderr = assert_command_failure(
+        str(LOC_TEST_DATA / "dummy_with_empty_delims.py"),
+        f"--instructions='{instructions}'",
+        "--use-local",
+    )
+    assert "The delimited block does not contain any code" in stderr
+
+
+@mark.test_ollama
+def test_work_with_empty_delims_2() -> None:
+    instructions = "Replace the variable `c` with the integer 3"
+    stderr = assert_command_failure(
+        str(LOC_TEST_DATA / "dummy_with_empty_delims_2.py"),
+        f"--instructions='{instructions}'",
+        "--use-local",
+    )
+    assert "The delimited block does not contain any code" in stderr
+
+
+@mark.test_ollama
+def test_work_on_unknown_file(outputted_script: str) -> None:
+    instructions = "Capitalize all words in the file"
+    assert_command_success(
+        str(LOC_TEST_DATA / "unknown_file_type.abc"),
+        f"--instructions='{instructions}'",
+        f"-o{outputted_script}",  # Will write to a .py file but it's okay
+        "--use-local",
+    )
+
+    with open(outputted_script) as f:
+        assert "Foobar" in f.read()
