22bc92f4113a2a81d1aabebc4ac6b01475a06d7c
dsw7 <dds3443@hotmail.com>
2026-01-05 23:56:54 -0800

    Rename

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -1,27 +1,9 @@
-from os import remove
-from subprocess import run, PIPE
-from typing import Generator
-from pytest import fixture
-from utils import LOC_TEST_DATA, assert_command_success, assert_command_failure
-
-
-@fixture
-def outputted_script() -> Generator[str, None, None]:
-    path_to_outputted_script = "/tmp/dummy_basic.py"
-    yield path_to_outputted_script
-
-    try:
-        remove(path_to_outputted_script)
-    except FileNotFoundError:
-        pass
-
-
-def run_outputted_python_script(python_script: str) -> str:
-    # some tests will edit a python script with LLMs and place the edited copy elsewhere
-    # run the edited copy to see if it works after modification by an LLM
-    process = run(["python3", python_script], stdout=PIPE, stderr=PIPE, text=True)
-    assert process.returncode == 0, process.stderr
-    return process.stdout
+from utils import (
+    LOC_TEST_DATA,
+    assert_command_success,
+    assert_command_failure,
+    assert_python_script_runs,
+)
 
 
 def test_read_instructions_from_file(outputted_script: str) -> None:
@@ -30,7 +12,7 @@ def test_read_instructions_from_file(outputted_script: str) -> None:
         f'--file={LOC_TEST_DATA / "edit.txt"}',
         f"-o{outputted_script}",
     )
-    assert "The sum is 14" in run_outputted_python_script(outputted_script)
+    assert "The sum is 14" in assert_python_script_runs(outputted_script)
 
 
 def test_read_instructions_from_cli(outputted_script: str) -> None:
@@ -41,7 +23,7 @@ def test_read_instructions_from_cli(outputted_script: str) -> None:
         f"-o{outputted_script}",
     )
     assert "Prompt:" not in stdout  # Prompt should not print by default
-    assert "The sum is 9" in run_outputted_python_script(outputted_script)
+    assert "The sum is 9" in assert_python_script_runs(outputted_script)
 
 
 def test_print_prompt_with_verbose_flag(outputted_script: str) -> None:
@@ -53,7 +35,7 @@ def test_print_prompt_with_verbose_flag(outputted_script: str) -> None:
         "--verbose",
     )
     assert "Prompt:" in stdout
-    assert "The sum is 9" in run_outputted_python_script(outputted_script)
+    assert "The sum is 9" in assert_python_script_runs(outputted_script)
 
 
 def test_only_edit_between_delims(outputted_script: str) -> None:
@@ -63,7 +45,7 @@ def test_only_edit_between_delims(outputted_script: str) -> None:
         f"--instructions='{instructions}'",
         f"-o{outputted_script}",
     )
-    assert "The sum is 17" in run_outputted_python_script(outputted_script)
+    assert "The sum is 17" in assert_python_script_runs(outputted_script)
 
 
 def test_bad_delim_placement(outputted_script: str) -> None:
--- tests/utils.py
+++ tests/utils.py
@@ -21,7 +21,7 @@ def assert_command_failure(*args: str) -> str:
     return process.stderr
 
 
-def run_outputted_python_script(python_script: str) -> str:
+def assert_python_script_runs(python_script: str) -> str:
     # some tests will edit a python script with LLMs and place the edited copy elsewhere
     # run the edited copy to see if it works after modification by an LLM
     process = run(["python3", python_script], stdout=PIPE, stderr=PIPE, text=True)
