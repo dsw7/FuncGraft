1826470de428a95bf4ff4394d7f0adfce6358a96
dsw7 <dds3443@hotmail.com>
2026-04-19 00:33:55 -0700

    Fix tests

--- tests/test_edit_openai.py
+++ tests/test_edit_openai.py
@@ -1,15 +1,11 @@
 from pytest import mark
-from utils import (
-    LOC_TEST_DATA,
-    assert_command_success,
-    assert_command_failure,
-    assert_python_script_runs,
-)
+from utils import LOC_TEST_DATA, assert_command_success, assert_python_script_runs
 
 
 @mark.test_openai
 def test_read_instructions_from_file(outputted_script: str) -> None:
     assert_command_success(
+        "--provider=openai",
         f"{LOC_TEST_DATA}/dummy_basic.py",
         f'--file={LOC_TEST_DATA / "edit.txt"}',
         f"-o{outputted_script}",
@@ -21,6 +17,7 @@ def test_read_instructions_from_file(outputted_script: str) -> None:
 def test_read_instructions_from_cli(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 5"
     stdout = assert_command_success(
+        "--provider=openai",
         f"{LOC_TEST_DATA}/dummy_basic.py",
         f"--instructions='{instructions}'",
         f"-o{outputted_script}",
@@ -33,6 +30,7 @@ def test_read_instructions_from_cli(outputted_script: str) -> None:
 def test_print_prompt_with_verbose_flag(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 5"
     stdout = assert_command_success(
+        "--provider=openai",
         f"{LOC_TEST_DATA}/dummy_basic.py",
         f"--instructions='{instructions}'",
         f"-o{outputted_script}",
@@ -46,6 +44,7 @@ def test_print_prompt_with_verbose_flag(outputted_script: str) -> None:
 def test_only_edit_between_delims(outputted_script: str) -> None:
     instructions = "Replace the variable `c` with the integer 3"
     assert_command_success(
+        "--provider=openai",
         f"{LOC_TEST_DATA}/dummy_with_delims.py",
         f"--instructions='{instructions}'",
         f"-o{outputted_script}",
@@ -57,10 +56,11 @@ def test_only_edit_between_delims(outputted_script: str) -> None:
 def test_work_on_unknown_file(outputted_script: str) -> None:
     instructions = "Capitalize all words in the file"
     assert_command_success(
+        "--provider=openai",
         f"{LOC_TEST_DATA}/unknown_file_type.abc",
         f"--instructions='{instructions}'",
         f"-o{outputted_script}",  # Will write to a .py file but it's okay
     )
 
-    with open(outputted_script) as f:
+    with open(outputted_script, encoding="utf-8") as f:
         assert "Foobar" in f.read()
