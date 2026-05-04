5d35efe4e6718604bc69d7b9d5cdb9960d1d8e55
dsw7 <dds3443@hotmail.com>
2026-05-04 00:04:06 -0700

    Add right justified print

--- src/console.cpp
+++ src/console.cpp
@@ -24,10 +24,19 @@ namespace console {
 
 void print_separator()
 {
-    static unsigned short columns = get_terminal_columns_();
-    static std::string separator = std::string(columns, '-');
+    static unsigned short width = get_terminal_columns_();
+    static std::string separator = std::string(width, '-');
 
     fmt::print(fg(fmt::color::gray), "{}\n", separator);
+}
+
+void print_right_align(const std::string &message)
+{
+    static unsigned short width = get_terminal_columns_();
+    static unsigned short r_pad = 2;
+
+    const unsigned short l_pad = width - r_pad;
+    fmt::print(fg(fmt::color::gray), "{:>{}}\n", message, l_pad);
 }
 
 } // namespace console
--- src/console.hpp
+++ src/console.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+
 namespace console {
 void print_separator();
-}
+void print_right_align(const std::string &message);
+} // namespace console
