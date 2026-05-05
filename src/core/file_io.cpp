07032594f6b884398039598ce16f291331ca7556
dsw7 <dds3443@hotmail.com>
2026-05-05 00:41:06 -0700

    Add class for managing file content

--- src/core/file_io.cpp
+++ src/core/file_io.cpp
@@ -9,6 +9,34 @@
 namespace core {
 namespace file_io {
 
+FileToEdit::FileToEdit(const std::filesystem::path &filename)
+{
+    if (not std::filesystem::exists(filename)) {
+        throw std::runtime_error(fmt::format("File '{}' does not exist!", filename.string()));
+    }
+
+    if (not std::filesystem::is_regular_file(filename)) {
+        throw std::runtime_error(fmt::format("Input '{}' is not a file!", filename.string()));
+    }
+
+    this->content_ = utils::read_from_file(filename);
+}
+
+std::string FileToEdit::get_file_content()
+{
+    return this->content_;
+}
+
+void FileToEdit::set_file_content(const std::string &content)
+{
+    this->content_ = content;
+}
+
+void FileToEdit::export_content(const std::filesystem::path &filename)
+{
+    utils::write_to_file(filename, this->content_);
+}
+
 std::string import_file_to_edit(const fs::path &filename)
 {
     if (not fs::exists(filename)) {
--- src/core/file_io.hpp
+++ src/core/file_io.hpp
@@ -7,6 +7,18 @@ namespace fs = std::filesystem;
 
 namespace core {
 namespace file_io {
+
+class FileToEdit {
+public:
+    FileToEdit(const std::filesystem::path &filename);
+    std::string get_file_content();
+    void set_file_content(const std::string &content);
+    void export_content(const std::filesystem::path &filename);
+
+private:
+    std::string content_;
+};
+
 std::string import_file_to_edit(const fs::path &filename);
 void export_edited_file(const std::string &code, const fs::path &filename);
 void export_edited_file_with_prompt(const std::string &code, const fs::path &filename);
