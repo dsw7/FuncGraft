61aca318933855b49c8252f0e99860c222cf7d8b
dsw7 <dds3443@hotmail.com>
2026-05-10 23:07:24 -0700

    Use inheritance

--- src/adapters/errors.cpp
+++ src/adapters/errors.cpp
@@ -1,49 +1,42 @@
 #include "errors.hpp"
 
 #include <fmt/core.h>
-#include <json.hpp>
 #include <stdexcept>
 
 namespace adapters {
 
-OpenAIError::OpenAIError(const std::string &response, const int status_code) :
+ErrorResponse::ErrorResponse(const std::string &response, const int status_code) :
     status_code(status_code)
 {
-    nlohmann::json json;
-
     try {
-        json = nlohmann::json::parse(response);
+        this->json_ = nlohmann::json::parse(response);
     } catch (const nlohmann::json::parse_error &e) {
         throw std::runtime_error(fmt::format("Failed to parse response: {}", e.what()));
     }
+}
 
-    if (not json.contains("error")) {
+OpenAIError::OpenAIError(const std::string &response, const int status_code) :
+    ErrorResponse(response, status_code)
+{
+    if (not this->json_.contains("error")) {
         throw std::runtime_error("An error occurred but 'error' key not found in the response JSON");
     }
 
-    if (not json["error"].contains("message")) {
+    if (not this->json_["error"].contains("message")) {
         throw std::runtime_error("An error occurred but 'error.message' not found in the response JSON");
     }
 
-    this->errmsg = json["error"]["message"];
+    this->errmsg = this->json_["error"]["message"];
 }
 
 OllamaError::OllamaError(const std::string &response, const int status_code) :
-    status_code(status_code)
+    ErrorResponse(response, status_code)
 {
-    nlohmann::json json;
-
-    try {
-        json = nlohmann::json::parse(response);
-    } catch (const nlohmann::json::parse_error &e) {
-        throw std::runtime_error(fmt::format("Failed to parse response: {}", e.what()));
-    }
-
-    if (not json.contains("error")) {
+    if (not this->json_.contains("error")) {
         throw std::runtime_error("An error occurred but 'error' key not found in the response JSON");
     }
 
-    this->errmsg = json["error"];
+    this->errmsg = this->json_["error"];
 }
 
 } // namespace adapters
--- src/adapters/errors.hpp
+++ src/adapters/errors.hpp
@@ -1,21 +1,26 @@
 #pragma once
 
+#include <json.hpp>
 #include <string>
 
 namespace adapters {
 
-struct OllamaError {
-    OllamaError(const std::string &response, const int status_code);
-
+class ErrorResponse {
+public:
+    ErrorResponse(const std::string &response, const int status_code);
     std::string errmsg;
     int status_code = 200;
+
+protected:
+    nlohmann::json json_;
 };
 
-struct OpenAIError {
-    OpenAIError(const std::string &response, const int status_code);
+struct OllamaError: public ErrorResponse {
+    OllamaError(const std::string &response, const int status_code);
+};
 
-    std::string errmsg;
-    int status_code = 200;
+struct OpenAIError: public ErrorResponse {
+    OpenAIError(const std::string &response, const int status_code);
 };
 
 } // namespace adapters
